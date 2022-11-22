f62c396f38e002d2710404383921dd3525b63915
OfficialYeet638 <101959968+OfficialYeet638@users.noreply.github.com>
2022-11-22 20:35:57 +0800

    v0.1.4

--- Source_code_v0.1.4.py
+++ Source_code_v0.1.4.py
@@ -1,10 +1,10 @@
-from ctypes import cast, POINTER, windll, Structure, c_long, byref
+import ctypes
+from ctypes import cast, POINTER, windll
 from comtypes import CLSCTX_ALL
 from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
 import time
-import webbrowser
-import ctypes
-import pygame
+import vlc
+import pafy
 import screen_brightness_control as sbc
 import os
 import cv2
@@ -30,9 +30,15 @@ volume.GetMasterVolumeLevel()
 volume.GetVolumeRange()
 volume.SetMasterVolumeLevel(0, None)
 
-#Play RickRoll!
-webbrowser.open('https://youtu.be/dQw4w9WgXcQ')
-time.sleep(1.3)
+#initialise for playing Rickroll
+url = "https://youtu.be/dQw4w9WgXcQ"
+video = pafy.new(url)
+best = video.streams[0]
+media = vlc.MediaPlayer(best.url)
+
+#Play RickRoll in full screen!
+media.play()
+media.toggle_fullscreen()
 
 #stunt ur eyes
 #get screen resolution
@@ -40,24 +46,11 @@ user32 = ctypes.windll.user32
 user32.SetProcessDPIAware()
 [w, h] = [user32.GetSystemMetrics(0), user32.GetSystemMetrics(1)]
 
-#set flashing window size
-screen = pygame.display.set_mode((w, h))
-
 #max screen brightness
 sbc.set_brightness(100)
 
-pygame.display.set_caption('window')
-running = True
 
 while True:
   sbc.set_brightness(100)
   volume.SetMute(0, None)
   volume.SetMasterVolumeLevel(0, None)
-  background_colour = (255,255,255)
-  screen.fill(background_colour)
-  pygame.display.flip()
-  time.sleep(0.03)
-  background_colour = (0,0,0)
-  screen.fill(background_colour)
-  pygame.display.flip()
-  time.sleep(0.03)
